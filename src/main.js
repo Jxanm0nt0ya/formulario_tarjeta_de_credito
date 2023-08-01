d1b9ec3c7e1af7ca5fe27de414a6873b5e01c64e
Jxanm0nt0ya <129190973+Jxanm0nt0ya@users.noreply.github.com>
2023-07-31 20:49:30 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -10,10 +10,26 @@ const cardMonth = document.querySelector("#exp-mes");
 const cardYear = document.querySelector("#exp-año");
 const cardCVC = document.querySelector("#tarjeta-cvc");
 const form = document.querySelector("#form");
-const thankYou = document.querySelector("#gracias");
+const thankYou = document.querySelector("#thank-you");
 const buttonContinue = document.querySelector("#continue");
 
+function soloLetras(e) {
+    let key = e.keyCode || e.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    let especiales = [8, 37, 39, 46];
+    let tecla_especial = false
+    for (let i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
 
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+    }
+}
 
 inputName.addEventListener("input", () => {
     cardName.innerText = inputName.value;
@@ -27,6 +43,27 @@ inputName.addEventListener("input", () => {
             this.value = this.value.slice(0, 24);
     })
 })
+
+function soloNumeros(e) {
+    var key = e.keyCode || e.which;
+    var tecla = String.fromCharCode(key);
+    var numeros = "0123456789";
+    var especiales = [8, 37, 39, 46];
+    var tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if (numeros.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+    }
+}
+
+
+
 
 inputNumber.addEventListener("input", () => {
     cardNumber.innerText = inputNumber.value;
@@ -81,4 +118,21 @@ inputCVC.addEventListener("input", () => {
             this.value = this.value.slice(0, 3);
     })
 
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.classList.add("disabled");
+    thankYou.classList.remove("disabled");
+})
+
+buttonContinue.addEventListener("click", () => {
+    form.classList.remove("disabled");
+    thankYou.classList.add("disabled");
+    form.reset();
+    cardName.innerText = "Jane Appleseed";
+    cardNumber.innerText = "0000 0000 0000 0000";
+    cardMonth.innerText = "00";
+    cardYear.innerText = "00";
+    cardCVC.innerText = "000";
 })
